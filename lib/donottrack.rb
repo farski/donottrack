dff403defe11aa5f0152c60c13246334ce837209
Chris Kalafarski <chris@farski.com>
2011-05-24 12:47:00 -0700

    Moved default into a constant, to be more obvious

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -1,12 +1,14 @@
 module Rack
   class DoNotTrack
+    DEFAULT_DNT_BIT = 0
+
     def initialize(app, dnt_bit = nil)
       @app = app
       @dnt_bit = dnt_bit
     end
 
     def call(env)
-      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)
+      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || DEFAULT_DNT_BIT)
       status, headers, response = @app.call(env)
       [status, headers, response]
     end
