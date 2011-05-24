e5f72694496d1c1ccc1953216dd124a3f9fee1c0
Chris Kalafarski <chris@farski.com>
2011-05-24 12:14:40 -0700

    Fixing some whitespace malformalities

--- lib/donottrack.rb
+++ lib/donottrack.rb
@@ -5,10 +5,10 @@ module Rack
       @dnt_bit = dnt_bit
     end
 
-		def call(env)
-		  env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)
-			status, headers, response = @app.call(env)
-			[status, headers, response]
+    def call(env)
+      env['HTTP_DNT'] = @dnt_bit ? @dnt_bit : (env['HTTP_DNT'] || env['HTTP_X_DO_NOT_TRACK'] || 0)
+      status, headers, response = @app.call(env)
+      [status, headers, response]
     end
   end
 end
